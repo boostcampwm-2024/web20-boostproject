27736a33507afe753ce5586996976ae87aff3346
g00hyun <rhkdgus1417@hufs.ac.kr>
2024-11-07 01:45:57 +0900

    :sparkles: [Feat] nest 실행 컨텍스트에 interceptor 및 filter 붙이기

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/interceptor/transform.interceptor';
+import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  await app.listen(3000);
 }
 bootstrap();
