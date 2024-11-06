73b691887266d4137ef8eb0bf40bda4b2408b067
g00hyun <rhkdgus1417@hufs.ac.kr>
2024-11-07 01:46:41 +0900

    :sparkles: [Feat] 응답 적용 확인을 위한 테스트용 컨트롤러

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CustomException } from './common/exceptions/custom.exception';
+import { ErrorTypes } from './common/exceptions/errorTypes';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,30 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  
+  @Get('/success/1')
+  getSuccessTest1(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/success/2')
+  getSuccessTest2(): Object {
+    return { name: 'KIM', age: 23 };
+  }
+
+  @Get('/error/1')
+  getErrorTest1(): string {
+    throw new CustomException(ErrorTypes.INTERNAL_SERVER_ERROR);
+  }
+
+  @Get('/error/2')
+  getErrorTest2(): string {
+    throw new CustomException(ErrorTypes.INVALID_TOKEN);
+  }
+
+  @Get('/error/3')
+  getErrorTest3(): string {
+    throw new CustomException(ErrorTypes.UNAUTHORIZED);
   }
 }
