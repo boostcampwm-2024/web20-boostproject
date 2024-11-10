efd7f82851399f7a6421d950f34f8eff8eff5d8e
zero0205 <etety39@naver.com>
2024-11-10 15:33:42 +0900

    :art: [Style]: 헤더의 이벤트 핸들러들의 prefix를 handle로 변경

--- apps/client/src/components/common/Header.tsx
+++ apps/client/src/components/common/Header.tsx
@@ -7,24 +7,24 @@ function Header() {
   const [isCheckedIn, setIsCheckedIn] = useState(false);
   const navigate = useNavigate();
 
-  const onClickLogo = () => {
+  const handleLogoClick = () => {
     navigate('/');
   };
 
-  const onClickLogIn = () => {
+  const handleLogInClick = () => {
     // TODO: 로그인 로직 구현
     alert('로그인 모달 창 구현 예정');
     setIsLogIn(true);
   };
 
-  const onClickLogOut = () => {
+  const handleLogOutClick = () => {
     // TODO: 로그아웃 로직 구현
     alert('로그아웃');
     setIsLogIn(false);
     navigate('/');
   };
 
-  const onClickCheckIn = () => {
+  const handleCheckInClick = () => {
     // TODO: 방송 시작 구현
     setIsCheckedIn(prev => !prev);
     alert('체크인!');
@@ -32,21 +32,21 @@ function Header() {
 
   return (
     <header className="px-10 py-6 flex justify-between">
-      <div className="text-text-strong font-nabla text-4xl hover:cursor-pointer" onClick={onClickLogo}>
+      <div className="text-text-strong font-nabla text-4xl hover:cursor-pointer" onClick={handleLogoClick}>
         Cam'<span className="text-text-point">On</span>
       </div>
       {isLogIn ? (
         <div className="flex gap-2">
           <Button
             className={isCheckedIn && 'bg-surface-brand-default hover:bg-surface-brand-alt'}
-            onClick={onClickCheckIn}
+            onClick={handleCheckInClick}
           >
             체크인
           </Button>
-          <Button onClick={onClickLogOut}>로그아웃</Button>
+          <Button onClick={handleLogOutClick}>로그아웃</Button>
         </div>
       ) : (
-        <Button className="bg-surface-brand-default hover:bg-surface-brand-alt" onClick={onClickLogIn}>
+        <Button className="bg-surface-brand-default hover:bg-surface-brand-alt" onClick={handleLogInClick}>
           로그인
         </Button>
       )}
