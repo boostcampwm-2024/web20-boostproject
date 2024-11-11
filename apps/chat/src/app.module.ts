2f90097067c54b9840fd911fcff94c8fdddbe93f
sunny <huiseon37@gmail.com>
2024-11-11 17:14:19 +0900

    :wrench: [Chore]: redis module 수정

--- apps/chat/src/app.module.ts
+++ apps/chat/src/app.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { RedisModuleConfig } from './redis/redis.module';
-
+import { RedisModule } from '@nestjs-modules/ioredis';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    RedisModuleConfig,
+    RedisModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ apps/chat/src/config/redis.config.ts
@@ -0,0 +1,8 @@
+import { ConfigService } from '@nestjs/config';
+import { RedisOptions } from 'ioredis';
+
+export const redisConfig = (configService: ConfigService): RedisOptions => ({
+  host: configService.get<string>('REDIS_HOST'),
+  port: configService.get<number>('REDIS_PORT'),
+  db: configService.get<number>('REDIS_CHAT'),
+});
