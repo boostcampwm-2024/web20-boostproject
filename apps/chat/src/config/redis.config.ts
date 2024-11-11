b120f0e3fcab44e8f021acea3689070297238877
sunny <huiseon37@gmail.com>
2024-11-11 16:27:54 +0900

    :wrench: [Chore]: redis config 파일 수정

--- apps/chat/src/config/redis.config.ts
+++ apps/chat/src/config/redis.config.ts
@@ -4,7 +4,5 @@ import { RedisOptions } from 'ioredis';
 export const redisConfig = (configService: ConfigService): RedisOptions => ({
   host: configService.get<string>('REDIS_HOST'),
   port: configService.get<number>('REDIS_PORT'),
-  password: configService.get<string>('REDIS_PASSWORD'),
   db: configService.get<number>('REDIS_DB'),
-  keyPrefix: configService.get<string>('REDIS_KEY_PREFIX'),
 });
