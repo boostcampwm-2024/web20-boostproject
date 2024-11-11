96cb92d04c018ffacfcd4b5f9a5fe0c6972f9d44
sunny <huiseon37@gmail.com>
2024-11-12 00:52:49 +0900

    :sparkles: [Feat]: producers key값 roomId 로 수정

--- apps/media/src/sfu/producer/producer.gateway.ts
+++ apps/media/src/sfu/producer/producer.gateway.ts
@@ -4,6 +4,7 @@ import { CreateProducerDto } from './create-producer.dto';
 import { ICreateProducerParams } from './producer.interface';
 import { Producer } from 'mediasoup/node/lib/Producer';
 import { RouterGateway } from '../router.gateway';
+import { TransportGateway } from '../transport.gateway';
 
 @WebSocketGateway()
 export class ProducerGateway {
@@ -11,14 +12,15 @@ export class ProducerGateway {
   server: Server;
   private producers: Map<string, Producer[]>;
 
-  constructor(private readonly routerGateway: RouterGateway) {}
+  constructor(private readonly routerGateway: RouterGateway, private readonly transportGateway: TransportGateway) {}
 
   @SubscribeMessage('createProducer')
   async handleCreateProducer(@MessageBody() data: CreateProducerDto) {
     try {
-      const { transportId, kind, rtpParameters } = data;
+      const { roomId, transportId, kind, rtpParameters } = data;
 
       const producer = await this.createProducer({
+        roomId,
         transportId,
         kind,
         rtpParameters,
@@ -35,14 +37,20 @@ export class ProducerGateway {
   }
 
   async createProducer(params: ICreateProducerParams): Promise<Producer> {
-    const { transportId, kind, rtpParameters } = params;
+    const { roomId, transportId, kind, rtpParameters } = params;
+
+    const router = this.routerGateway.getRouter(roomId);
+
+    if (!router) {
+      throw new Error(`Room not found: ${roomId}`);
+    }
 
     // TODO: transport쪽에서 transport를 가져오는 로직 필요
     const transport = await this.transportGateway.getTransport(transportId);
 
     if (!transport) {
       // CustomException 구현 필요
-      throw new Error(`Transport not found: ${params.transportId}`);
+      throw new Error(`Transport not found: ${transportId}`);
     }
 
     const producer = await transport.produce({
@@ -50,10 +58,10 @@ export class ProducerGateway {
       rtpParameters,
     });
 
-    if (!this.producers.has(producer.id)) {
-      this.producers.set(producer.id, []);
+    if (!this.producers.has(roomId)) {
+      this.producers.set(roomId, []);
     }
-    this.producers.get(producer.id).push(producer);
+    this.producers.get(roomId).push(producer);
 
     return producer;
   }
