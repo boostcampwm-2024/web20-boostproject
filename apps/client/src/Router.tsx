8ae4db54d2b0a8ba098d59da7fddd699012ada1f
zero0205 <etety39@naver.com>
2024-11-12 14:43:35 +0900

    :bug: [Fix]: react Router v7 관련 옵션 추가

--- apps/client/src/Router.tsx
+++ apps/client/src/Router.tsx
@@ -5,29 +5,43 @@ import Profile from '@pages/Profile';
 import Live from '@pages/Live';
 import Broadcast from './pages/Broadcast';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '',
-        element: <Home />,
-      },
-      {
-        path: 'profile',
-        element: <Profile />,
-      },
-      {
-        path: 'live',
-        element: <Live />,
-      },
-    ],
+const routerOptions = {
+  future: {
+    v7_startTransition: true,
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
   },
-  {
-    path: 'broadcast',
-    element: <Broadcast />,
-  },
-]);
+};
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          path: '',
+          element: <Home />,
+        },
+        {
+          path: 'profile',
+          element: <Profile />,
+        },
+        {
+          path: 'live',
+          element: <Live />,
+        },
+      ],
+    },
+    {
+      path: 'broadcast',
+      element: <Broadcast />,
+    },
+  ],
+  routerOptions,
+);
 
 export default router;
