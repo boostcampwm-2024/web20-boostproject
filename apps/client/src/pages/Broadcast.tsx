026034efc6ce587da3771c9b78fa40040c6a4c9a
zero0205 <etety39@naver.com>
2024-11-12 12:10:58 +0900

    :recycle: [Refactor]: Broadcast 페이지 return문 하나만 나오도록 수정

--- apps/client/src/pages/Broadcast.tsx
+++ apps/client/src/pages/Broadcast.tsx
@@ -17,44 +17,44 @@ function Broadcast() {
     window.close();
   };
 
-  if (error) {
-    return (
-      <div className="flex flex-col">
-        <h2 className="text-display-bold24 text-text-danger">미디어 스트림을 불러오는데 실패했습니다.</h2>
-        <div className="text-display-medium16 text-text-danger">error</div>
-      </div>
-    );
-  }
-
   return (
-    <div className="">
-      <div className="w-full aspect-video p-4">
-        <video ref={videoRef} autoPlay muted className="w-full h-full bg-surface-alt rounded-xl" />
-        <audio />
-      </div>
-      <div className="w-full">
-        <div className="flex flex-row justify-between p-4">
-          <div className="text-text-default text-display-medium16">방송 제목</div>
-          <Button className="bg-transparent border border-border-default">수정</Button>
+    <>
+      {error ? (
+        <div className="flex flex-col">
+          <h2 className="text-display-bold24 text-text-danger">미디어 스트림을 불러오는데 실패했습니다.</h2>
+          <div className="text-display-medium16 text-text-danger">error</div>
         </div>
-        <div className="flex justify-between items-center m-4">
-          <Button onClick={handleCheckout} className="bg-surface-brand-default hover:hover:bg-surface-brand-alt">
-            체크아웃
-          </Button>
-          <div className="flex items-center gap-4">
-            <button onClick={toggleVideo}>{isVideoEnabled ? <VideoOnIcon /> : <VideoOffIcon />}</button>
-            <button onClick={toggleAudio}>{isAudioEnabled ? <MicrophoneOnIcon /> : <MicrophoneOffIcon />}</button>
-            <button>
-              <MonitorShareIcon />
-            </button>
-            <button>
-              <ChatIcon />
-            </button>
+      ) : (
+        <>
+          <div className="w-full aspect-video p-4">
+            <video ref={videoRef} autoPlay muted className="w-full h-full bg-surface-alt rounded-xl" />
+            <audio />
           </div>
-        </div>
-      </div>
-      <div className="border border-border-default rounded-xl"></div>
-    </div>
+          <div className="w-full">
+            <div className="flex flex-row justify-between p-4">
+              <div className="text-text-default text-display-medium16">방송 제목</div>
+              <Button className="bg-transparent border border-border-default">수정</Button>
+            </div>
+            <div className="flex justify-between items-center m-4">
+              <Button onClick={handleCheckout} className="bg-surface-brand-default hover:hover:bg-surface-brand-alt">
+                체크아웃
+              </Button>
+              <div className="flex items-center gap-4">
+                <button onClick={toggleVideo}>{isVideoEnabled ? <VideoOnIcon /> : <VideoOffIcon />}</button>
+                <button onClick={toggleAudio}>{isAudioEnabled ? <MicrophoneOnIcon /> : <MicrophoneOffIcon />}</button>
+                <button>
+                  <MonitorShareIcon />
+                </button>
+                <button>
+                  <ChatIcon />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="border border-border-default rounded-xl"></div>
+        </>
+      )}
+    </>
   );
 }
 
