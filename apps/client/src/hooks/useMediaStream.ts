93d977dd660d054123d53b47e82fb4ccd9d2f104
zero0205 <etety39@naver.com>
2024-11-12 12:04:35 +0900

    :recycle: [Refactor]: initializeStream을 useEffect 외부로 이동

--- apps/client/src/hooks/useMediaStream.ts
+++ apps/client/src/hooks/useMediaStream.ts
@@ -10,42 +10,40 @@ export const useMediaStream = () => {
   const streamRef = useRef<MediaStream | null>(null);
   const [mediaStramState, setMediaStreamState] = useState<MediaStreamState>({ mediaStream: null, error: null });
 
-  useEffect(() => {
-    const videoElement = videoRef.current;
+  const initializeStream = async () => {
+    try {
+      const constraints = {
+        video: true,
+        audio: true,
+      };
 
-    const initializeStream = async () => {
-      try {
-        const constraints = {
-          video: true,
-          audio: true,
-        };
+      const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
 
-        const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
-
-        if (videoElement) {
-          videoElement.srcObject = mediaStream;
-        }
-        streamRef.current = mediaStream;
-        setMediaStreamState({
-          mediaStream: mediaStream,
-          error: null,
-        });
-      } catch (err) {
-        setMediaStreamState({
-          mediaStream: null,
-          error: err instanceof Error ? err : new Error('Failed to get user media'),
-        });
+      if (videoRef.current) {
+        videoRef.current.srcObject = mediaStream;
       }
-    };
+      streamRef.current = mediaStream;
+      setMediaStreamState({
+        mediaStream: mediaStream,
+        error: null,
+      });
+    } catch (err) {
+      setMediaStreamState({
+        mediaStream: null,
+        error: err instanceof Error ? err : new Error('Failed to get user media'),
+      });
+    }
+  };
 
+  useEffect(() => {
     initializeStream();
 
     return () => {
       if (streamRef.current) {
         streamRef.current.getTracks().forEach(track => track.stop());
       }
-      if (videoElement) {
-        videoElement.srcObject = null;
+      if (videoRef.current) {
+        videoRef.current.srcObject = null;
       }
     };
   }, []);
