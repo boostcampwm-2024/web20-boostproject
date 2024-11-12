a4fc5d8244070e6f9f57d46f08cab02628bb465e
zero0205 <etety39@naver.com>
2024-11-12 19:22:04 +0900

    :bug: [Fix]: shadcn/ui 버튼 수정

--- apps/client/src/components/ui/button.tsx
+++ apps/client/src/components/ui/button.tsx
@@ -39,11 +39,12 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, Var
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, textSize, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
-    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+    return <Comp className={cn(buttonVariants({ variant, size, textSize, className }))} ref={ref} {...props} />;
   },
 );
 Button.displayName = 'Button';
 
-export { Button, buttonVariants, ButtonProps };
+export { Button, type ButtonProps };
+export { buttonVariants };
