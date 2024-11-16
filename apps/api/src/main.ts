9a596b7729e26a1f887bb7bbf86992fa09d023c9
sunny <huiseon37@gmail.com>
2024-11-17 03:04:57 +0900

    :wrench: [Chore]: swagger 세팅

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptor/transform.interceptor';
 import { HttpExceptionFilter } from './common/filter/httpException.filter';
+import { ConfigSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  ConfigSwagger.setup(app);
 
   await app.listen(3000);
 }
