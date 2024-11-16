b5fe71615673910b0063dd9836f5e66e872e5d88
zero0205 <etety39@naver.com>
2024-11-16 20:36:43 +0900

    :sparkles: [Feat]: 방송 리스트 레이아웃 UI 구현

--- apps/client/src/pages/Home.tsx
+++ apps/client/src/pages/Home.tsx
@@ -1,34 +1,100 @@
-import LiveCard from '@/components/LiveCard';
+import LiveList from '@/components/LiveList';
+
+const liveInfos = [
+  {
+    broadcastId: 'cbf4b8de0ce38a7c8c8c37e4eda54889',
+    broadcastTitle: '방송 제목 1',
+    thumbnail: '/images/zoom_membership_bg.png',
+    camperId: 'J001',
+    profileImage: '/images/duck.png',
+    field: 'WEB',
+  },
+  {
+    broadcastId: 'cbf4b8de0ce38a7c8c8c37e4eda54889',
+    broadcastTitle: '방송 제목 22',
+    thumbnail: '/images/zoom_membership_bg.png',
+    camperId: 'J225',
+    profileImage: '/images/duck.png',
+    field: 'WEB',
+  },
+  {
+    broadcastId: 'cbf4b8de0ce38a7c8c8c37e4eda54889',
+    broadcastTitle: '방송 제목 333',
+    thumbnail: '/images/zoom_membership_bg.png',
+    camperId: 'J389',
+    profileImage: '/images/duck.png',
+    field: 'AND',
+  },
+  {
+    broadcastId: 'cbf4b8de0ce38a7c8c8c37e4eda54889',
+    broadcastTitle: '방송 제목 4444',
+    thumbnail: '/images/zoom_membership_bg.png',
+    camperId: 'S486',
+    profileImage: '/images/duck.png',
+    field: 'IOS',
+  },
+  {
+    broadcastId: 'cbf4b8de0ce38a7c8c8c37e4eda54889',
+    broadcastTitle: '방송 제목 55555',
+    thumbnail: '/images/zoom_membership_bg.png',
+    camperId: 'J023',
+    profileImage: '/images/duck.png',
+    field: 'WEB',
+  },
+  {
+    broadcastId: 'cbf4b8de0ce38a7c8c8c37e4eda54889',
+    broadcastTitle: '방송 제목 666666',
+    thumbnail: '/images/zoom_membership_bg.png',
+    camperId: 'J126',
+    profileImage: '/images/duck.png',
+    field: 'WEB',
+  },
+  {
+    broadcastId: 'cbf4b8de0ce38a7c8c8c37e4eda54889',
+    broadcastTitle: '방송 제목 7777777',
+    thumbnail: '/images/zoom_membership_bg.png',
+    camperId: 'J219',
+    profileImage: '/images/duck.png',
+    field: 'WEB',
+  },
+  {
+    broadcastId: 'cbf4b8de0ce38a7c8c8c37e4eda54889',
+    broadcastTitle: '방송 제목 88888888',
+    thumbnail: '/images/zoom_membership_bg.png',
+    camperId: 'J273',
+    profileImage: '/images/duck.png',
+    field: 'IOS',
+  },
+  {
+    broadcastId: 'cbf4b8de0ce38a7c8c8c37e4eda54889',
+    broadcastTitle: '방송 제목 999999999',
+    thumbnail: '/images/zoom_membership_bg.png',
+    camperId: 'K101',
+    profileImage: '/images/duck.png',
+    field: 'AND',
+  },
+  {
+    broadcastId: 'cbf4b8de0ce38a7c8c8c37e4eda54889',
+    broadcastTitle: '방송 제목 테스트 123456789',
+    thumbnail: '/images/zoom_membership_bg.png',
+    camperId: 'S202',
+    profileImage: '/images/duck.png',
+    field: 'IOS',
+  },
+  {
+    broadcastId: 'cbf4b8de0ce38a7c8c8c37e4eda54889',
+    broadcastTitle: '방송 제목 테스트 123456789123456789',
+    thumbnail: '/images/zoom_membership_bg.png',
+    camperId: 'J299',
+    profileImage: '/images/duck.png',
+    field: 'WEB',
+  },
+];
 
 export default function Home() {
-  const liveInfos = [
-    {
-      id: 1,
-      title: '방송 제목1',
-      userId: 'J999',
-      profileUrl: '/images/duck.png',
-      thumbnailUrl: '/images/zoom_membership_bg.png',
-    },
-    { id: 2, title: '방송 제목2222222222222222222222', userId: 'K888', profileUrl: '', thumbnailUrl: '' },
-    { id: 3, title: '방송 제목3', userId: 'S777', profileUrl: '', thumbnailUrl: '' },
-    { id: 4, title: '방송 제목4', userId: 'J666', profileUrl: '', thumbnailUrl: '' },
-    { id: 5, title: '방송 제목5555555555555', userId: 'J555', profileUrl: '', thumbnailUrl: '' },
-    { id: 6, title: '방송 제목6', userId: 'J444', profileUrl: '', thumbnailUrl: '' },
-    { id: 7, title: '방송 제목7', userId: 'J333', profileUrl: '', thumbnailUrl: '' },
-    { id: 8, title: '방송 제목8', userId: 'J222', profileUrl: '', thumbnailUrl: '' },
-    { id: 9, title: '방송 제목9', userId: 'J111', profileUrl: '', thumbnailUrl: '' },
-  ];
   return (
-    <div className="flex flex-wrap p-8 gap-10">
-      {liveInfos.map(liveInfo => (
-        <LiveCard
-          key={liveInfo.id}
-          title={liveInfo.title}
-          userId={liveInfo.userId}
-          profileUrl={liveInfo.profileUrl}
-          thumbnailUrl={liveInfo.thumbnailUrl}
-        />
-      ))}
+    <div className="flex justify-center w-full">
+      <LiveList liveInfos={liveInfos} />
     </div>
   );
 }
