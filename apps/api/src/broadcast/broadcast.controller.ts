e26c10bc5854131e26cf84e73bf9a8fa8fd6e0e5
sunny <huiseon37@gmail.com>
2024-11-18 01:43:00 +0900

    :sparkles: [Feat]: 방송 정보 제공 api 구현

--- apps/api/src/broadcast/broadcast.controller.ts
+++ apps/api/src/broadcast/broadcast.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { BroadcastService } from './broadcast.service';
 import { SuccessStatus } from '../common/responses/bases/successStatus';
 import { BroadcastListResponseDto } from './dto/broadcast-list-response.dto';
+import { BroadcastInfoDto } from './dto/broadcast-info.dto';
 
 @Controller('broadcasts')
 export class BroadcastController {
@@ -11,5 +12,12 @@ export class BroadcastController {
   async getAll() {
     const broadcasts = await this.broadcastService.getAll();
     return SuccessStatus.OK(BroadcastListResponseDto.from(broadcasts));
+  }
+
+  @Get('/:broadcastId/info')
+  async getBroadcastInfo(@Param('broadcastId') broadcastId: string) {
+    const broadcast = await this.broadcastService.getBroadcastInfo(broadcastId);
+
+    return SuccessStatus.OK(BroadcastInfoDto.from(broadcast));
   }
 }
--- apps/api/src/broadcast/broadcast.service.ts
+++ apps/api/src/broadcast/broadcast.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { Broadcast } from './broadcast.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CustomException } from 'src/common/responses/exceptions/custom.exception';
+import { ErrorStatus } from 'src/common/responses/exceptions/errorStatus';
 
 @Injectable()
 export class BroadcastService {
@@ -12,5 +14,17 @@ export class BroadcastService {
       .createQueryBuilder('broadcast')
       .leftJoinAndSelect('broadcast.member', 'member')
       .getMany();
+  }
+
+  async getBroadcastInfo(broadcastId: string) {
+    const broadcast = await this.broadcastRepository
+      .createQueryBuilder('broadcast')
+      .leftJoinAndSelect('broadcast.member', 'member')
+      .where('broadcast.id = :broadcastId', { broadcastId })
+      .getOne();
+
+    if (!broadcast) throw new CustomException(ErrorStatus.BROADCAST_NOT_FOUND);
+
+    return broadcast;
   }
 }
