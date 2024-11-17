b43676b025618b334ee94216191591bc2d9c4593
g00hyun <rhkdgus1417@hufs.ac.kr>
2024-11-17 23:44:58 +0900

    :sparkles: [Feat]: plain pipe 추가

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptor/transform.interceptor';
 import { HttpExceptionFilter } from './common/filter/httpException.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(3000);
