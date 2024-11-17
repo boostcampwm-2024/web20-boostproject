1d36ab6ac004723107917274b30a633892cd86e6
sunny <huiseon37@gmail.com>
2024-11-17 22:18:11 +0900

    :sparkles: [Feat]: error 응답 데코레이터 수정

--- apps/api/src/common/decorators/error-res.decorator.ts
+++ apps/api/src/common/decorators/error-res.decorator.ts
@@ -2,31 +2,49 @@ import { applyDecorators } from '@nestjs/common';
 import { ApiExtraModels, ApiProperty, ApiResponse, getSchemaPath } from '@nestjs/swagger';
 import { ErrorStatus } from '../responses/exceptions/errorStatus';
 
-const createErrorResponseClass = (options: ErrorStatus) => {
+const createErrorResponseClass = () => {
   class ErrorResponse {
-    @ApiProperty({ example: options.code || 500 })
+    @ApiProperty()
     code: number;
 
-    @ApiProperty({ example: options.status || 'COMMON_5000' })
+    @ApiProperty()
     status: string;
 
-    @ApiProperty({ example: options.message || '실패' })
+    @ApiProperty()
     message: string;
   }
 
   return ErrorResponse;
 };
 
-export const ApiErrorResponse = (options: ErrorStatus) => {
-  const ErrorResponse = createErrorResponseClass(options);
+export const ApiErrorResponse = (errorCode: number, ...optionsList: ErrorStatus[]) => {
+  const ErrorResponse = createErrorResponseClass();
+
+  const examples = optionsList.reduce((acc, options) => {
+    acc[options.status] = {
+      summary: options.status,
+      value: {
+        code: options.code,
+        status: options.status,
+        message: options.message,
+      },
+      description: options.message,
+    };
+    return acc;
+  }, {} as Record<string, unknown>);
 
   return applyDecorators(
     ApiExtraModels(ErrorResponse),
     ApiResponse({
-      description: options.message || '실패',
-      status: options.code,
-      schema: {
-        allOf: [{ $ref: getSchemaPath(ErrorResponse) }],
+      description: `${errorCode}코드 에러`,
+      status: errorCode,
+      content: {
+        'application/json': {
+          schema: {
+            allOf: [{ $ref: getSchemaPath(ErrorResponse) }],
+          },
+          examples,
+        },
       },
     }),
   );
