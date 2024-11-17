8941721dacc896ba83103852a08a9aa73ce13ada
zero0205 <etety39@naver.com>
2024-11-17 22:49:59 +0900

    :sparkles: [Feat]: LiveInfo 타입 정의

--- apps/client/src/components/LiveList.tsx
+++ apps/client/src/components/LiveList.tsx
@@ -1,20 +1,13 @@
+import { LiveInfo } from '@/types/liveTypes';
 import LiveCard from './LiveCard';
 
-interface LiveInfo {
-  broadcastId: string;
-  broadcastTitle: string;
-  thumbnail: string;
-  camperId: string;
-  profileImage: string;
-  field: string;
-}
-
-function LiveList({ liveInfos }: { liveInfos: LiveInfo[] }) {
+function LiveList({ liveList }: { liveList: LiveInfo[] }) {
   return (
     <div className="grid grid-cols-1 min-[690px]:grid-cols-2 min-[1040px]:grid-cols-3 min-[1380px]:grid-cols-4 min-[1720px]:grid-cols-5 gap-[clamp(40px,2vw,60px)] p-15 w-[95%] max-w-[1920px] place-items-center">
-      {liveInfos.map(liveInfo => (
+      {liveList.map(liveInfo => (
         <LiveCard
           key={liveInfo.broadcastId}
+          liveId={liveInfo.broadcastId}
           title={liveInfo.broadcastTitle}
           userId={liveInfo.camperId}
           profileUrl={liveInfo.profileImage}
