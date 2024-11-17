5e1ea43fdaa7ce62b86f60fc2960859d333ee091
zero0205 <etety39@naver.com>
2024-11-18 01:47:58 +0900

    :lipstick: [Design]: 아이콘 수정 및 추가

--- apps/client/src/components/icons/BlogIcon.tsx
+++ apps/client/src/components/icons/BlogIcon.tsx
@@ -1,4 +1,9 @@
-function BlogIcon() {
+interface IconProps {
+  size?: number;
+  className?: string;
+}
+
+function BlogIcon({ size = 24, className = '' }: IconProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -6,7 +11,9 @@ function BlogIcon() {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="size-6"
+      width={size}
+      height={size}
+      className={className}
     >
       <path
         strokeLinecap="round"
