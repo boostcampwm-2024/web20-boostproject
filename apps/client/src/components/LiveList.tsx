b61d0299d6f794fc05d229f9223b2556a495cd3c
zero0205 <etety39@naver.com>
2024-11-19 00:14:58 +0900

    :recycle: [Refactor]: useAPI 사용하도록 변경

--- apps/client/src/components/LiveList.tsx
+++ apps/client/src/components/LiveList.tsx
@@ -1,19 +1,20 @@
 import { LiveInfo } from '@/types/liveTypes';
 import LiveCard from './LiveCard';
 
-function LiveList({ liveList }: { liveList: LiveInfo[] }) {
+function LiveList({ liveList }: { liveList: LiveInfo[] | null }) {
   return (
     <div className="grid grid-cols-1 min-[690px]:grid-cols-2 min-[1040px]:grid-cols-3 min-[1380px]:grid-cols-4 min-[1720px]:grid-cols-5 gap-[clamp(40px,2vw,60px)] p-15 w-[95%] max-w-[1920px] place-items-center">
-      {liveList.map(liveInfo => (
-        <LiveCard
-          key={liveInfo.broadcastId}
-          liveId={liveInfo.broadcastId}
-          title={liveInfo.broadcastTitle}
-          userId={liveInfo.camperId}
-          profileUrl={liveInfo.profileImage}
-          thumbnailUrl={liveInfo.thumbnail}
-        />
-      ))}
+      {liveList &&
+        liveList.map(liveInfo => (
+          <LiveCard
+            key={liveInfo.broadcastId}
+            liveId={liveInfo.broadcastId}
+            title={liveInfo.broadcastTitle}
+            userId={liveInfo.camperId}
+            profileUrl={liveInfo.profileImage}
+            thumbnailUrl={liveInfo.thumbnail}
+          />
+        ))}
     </div>
   );
 }
--- apps/client/src/pages/Home.tsx
+++ apps/client/src/pages/Home.tsx
@@ -1,37 +1,16 @@
-import { LiveInfo } from '@/types/liveTypes';
 import LiveList from '@/components/LiveList';
-
-import { useEffect, useState } from 'react';
 import LoadingCharacter from '@components/common/LoadingCharacter';
 import ErrorCharacter from '@components/common/ErrorCharacter';
-import { getLiveList } from '@/services/axios';
+import { useAPI } from '@/hooks/useAPI';
 
 export default function Home() {
-  const [liveList, setLiveList] = useState<LiveInfo[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<string>('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getLiveList();
-        setLiveList(data);
-      } catch (err) {
-        console.error('Failed to fetch live data: ', err);
-        setError('방송 목록을 불러오는데 실패했습니다.');
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const { data: liveList, isLoading, error } = useAPI({ url: '/v1/broadcasts' });
 
   return (
     <div className="flex justify-center w-full h-[calc(100vh-88px)]">
       {error ? (
         <div className="flex justify-center items-center flex-1">
-          <ErrorCharacter message={error} />
+          <ErrorCharacter message={error.message} />
         </div>
       ) : isLoading ? (
         <div className="flex justify-center items-center flex-1">
