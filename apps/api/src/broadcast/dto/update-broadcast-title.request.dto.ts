09ef28168e6e2b0cf9c43743d00b909ca1e85a29
sunny <huiseon37@gmail.com>
2024-11-19 12:00:38 +0900

    :sparkles: [Feat]: custom validation pipe 구현

--- apps/api/src/broadcast/dto/update-broadcast-title.request.dto.ts
+++ apps/api/src/broadcast/dto/update-broadcast-title.request.dto.ts
@@ -1,7 +1,7 @@
 import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class UpdateBroadcastTitleDto {
-  @IsString()
+  @IsString({ message: '제목은 string 형식이어야 합니다.' })
   @MinLength(1, { message: '제목은 최소 1자 이상이어야 합니다.' })
   @MaxLength(255, { message: '제목 글자 수 제한을 초과했습니다.' })
   title: string;
