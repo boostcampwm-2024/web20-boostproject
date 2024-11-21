78f69d9445c04697ed8a25f1508d7b51ca46358c
zero0205 <etety39@naver.com>
2024-11-22 01:23:34 +0900

    :lipstick: [Design]: home에서 방송 목록 grid 스타일 수정

--- apps/client/src/components/LiveList.tsx
+++ apps/client/src/components/LiveList.tsx
@@ -3,19 +3,20 @@ import LiveCard from './LiveCard';
 
 function LiveList({ liveList }: { liveList: LivePreviewInfo[] }) {
   return (
-    <div className="grid grid-cols-1 min-[690px]:grid-cols-2 min-[1040px]:grid-cols-3 min-[1380px]:grid-cols-4 min-[1720px]:grid-cols-5 gap-[clamp(40px,2vw,60px)] p-15 w-[95%] max-w-[1920px] place-items-center">
+    <div className="grid grid-cols-1 min-[690px]:grid-cols-2 min-[1040px]:grid-cols-3 min-[1380px]:grid-cols-4 min-[1720px]:grid-cols-5 gap-x-[clamp(40px,2vw,60px)] gap-y-12 auto-rows-min p-15 w-[95%] max-w-[1920px] align-items-start">
       {liveList &&
         liveList.map(livePreviewInfo => {
           const { broadcastId, broadcastTitle, camperId, profileImage, thumbnail } = livePreviewInfo;
           return (
-            <LiveCard
-              key={broadcastId}
-              liveId={broadcastId}
-              title={broadcastTitle}
-              userId={camperId}
-              profileUrl={profileImage}
-              thumbnailUrl={thumbnail}
-            />
+            <div key={broadcastId} className="flex justify-center">
+              <LiveCard
+                liveId={broadcastId}
+                title={broadcastTitle}
+                userId={camperId}
+                profileUrl={profileImage}
+                thumbnailUrl={thumbnail}
+              />
+            </div>
           );
         })}
     </div>
