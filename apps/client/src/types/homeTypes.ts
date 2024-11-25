a92f3a9092c6c2371d7888b6906b610759f47aad
sunny <huiseon37@gmail.com>
2024-11-26 01:39:57 +0900

    :sparkles: [Feat]: 프론트엔드 방송 리스트 조회 코드 수정

--- apps/client/src/types/homeTypes.ts
+++ apps/client/src/types/homeTypes.ts
@@ -1,8 +1,13 @@
-export interface LivePreviewInfo {
+interface LivePreviewInfo {
   broadcastId: string;
   broadcastTitle: string;
   camperId: string;
   profileImage: string;
   thumbnail: string;
   field: 'WEB' | 'AND' | 'IOS';
+}
+
+export interface LivePreviewListInfo {
+  broadcasts: LivePreviewInfo[];
+  nextCursor: string | null;
 }
