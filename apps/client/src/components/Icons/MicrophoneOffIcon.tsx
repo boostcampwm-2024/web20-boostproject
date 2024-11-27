c18b4182d427670359b093b271f4c10c3fdce202
zero0205 <etety39@naver.com>
2024-11-28 02:52:56 +0900

    :lipstick: [Design]: 아이콘 수정 및 추가

--- apps/client/src/components/Icons/MicrophoneOffIcon.tsx
+++ apps/client/src/components/Icons/MicrophoneOffIcon.tsx
@@ -4,7 +4,7 @@ function MicrophoneOffIcon() {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={1.5}
+      strokeWidth={2}
       stroke="currentColor"
       className="size-6"
     >
