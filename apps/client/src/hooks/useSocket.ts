a4d8aadc3cfb0877be3acc0be9026a31f6076af7
zero0205 <etety39@naver.com>
2024-11-27 17:53:14 +0900

    :bug: [Fix]: 소켓에 accessToken 전달하는 방법 수정

--- apps/client/src/hooks/useSocket.ts
+++ apps/client/src/hooks/useSocket.ts
@@ -27,8 +27,8 @@ export const useSocket = (url: string) => {
       reconnectionAttempts: 5, // 최대 재연결 시도 횟수
       reconnectionDelay: 1000,
       path: '/socket.io',
-      extraHeaders: {
-        Authorization: accessToken ? `Bearer ${accessToken}` : '',
+      auth: {
+        accessToken: accessToken ? `Bearer ${accessToken}` : '',
       },
     });
     socketRef.current = socket;
