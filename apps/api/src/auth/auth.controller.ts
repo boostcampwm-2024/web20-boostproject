7f8dd6e32950b4ee7a081ec7c8b619ae63ab3116
g00hyun <rhkdgus1417@hufs.ac.kr>
2024-11-27 11:11:07 +0900

    :sparkles: [Feat]: 로그인 리다이렉트 설정

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { AuthService } from './auth.service';
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Res, UseGuards } from '@nestjs/common';
 import { GithubAuthGuard } from './guard/github-auth.guard';
 import { GoogleAuthGuard } from './guard/google-auth.guard';
 import { ApiTags } from '@nestjs/swagger';
@@ -9,10 +9,12 @@ import { SuccessStatus } from 'src/common/responses/bases/successStatus';
 import { SigninResponseDto } from './dto/signin-response.dto';
 import { UserReq } from 'src/common/decorators/user-req.decorator';
 import { Member } from 'src/member/member.entity';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('/v1/auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService, private readonly configService: ConfigService) {}
 
   @Get('/signin/github')
   @UseGuards(GithubAuthGuard)
@@ -22,10 +24,11 @@ export class AuthController {
 
   @Get('/github/callback')
   @UseGuards(GithubAuthGuard)
-  signinGithubCallback(@UserReq() member: Member) {
+  signinGithubCallback(@UserReq() member: Member, @Res() res: Response) {
     const accessToken = this.authService.login(member);
+    const CALLBACK_URI = this.configService.get('CALLBACK_URI');
 
-    return { accessToken };
+    res.redirect(`${CALLBACK_URI}/auth?accessToken=${accessToken}`);
   }
 
   @Get('/signin/google')
@@ -36,9 +39,10 @@ export class AuthController {
 
   @Get('/google/callback')
   @UseGuards(GoogleAuthGuard)
-  signinGoogleCallback(@UserReq() member: Member) {
+  signinGoogleCallback(@UserReq() member: Member, @Res() res: Response) {
     const accessToken = this.authService.login(member);
+    const CALLBACK_URI = this.configService.get('CALLBACK_URI');
 
-    return { accessToken };
+    res.redirect(`${CALLBACK_URI}/auth?accessToken=${accessToken}`);
   }
 }
