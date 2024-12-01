1487224dd660f037ac6f87cf1d7bf77748ce318d
zero0205 <etety39@naver.com>
2024-12-02 00:48:06 +0900

    :sparkles: [Feat]: 검색 구현

--- apps/client/src/pages/Home/LiveList.tsx
+++ apps/client/src/pages/Home/LiveList.tsx
@@ -22,11 +22,19 @@ function LiveList() {
     setField(selected);
   };
 
+  const handleSearch = (keyword: string) => {
+    axiosInstance.get('v1/broadcasts/search', { params: { keyword: keyword.trim() } }).then(response => {
+      if (response.data.success) {
+        setLiveList(response.data.data);
+      }
+    });
+  };
+
   return (
     <div className="flex flex-col w-full h-full p-10">
       <div className="h-14 w-full flex justify-between items-center my-5 px-5">
         <FieldFilter selectedField={field} onFieldSelect={handleSelectField} />
-        <Search />
+        <Search onSearch={handleSearch} />
       </div>
       <div className="grid grid-cols-1 min-[690px]:grid-cols-2 min-[1040px]:grid-cols-3 min-[1380px]:grid-cols-4 min-[1720px]:grid-cols-5 gap-x-[clamp(40px,2vw,60px)] gap-y-12 auto-rows-min p-15 w-[95%] max-w-[1920px] align-items-start">
         {liveList ? (
--- apps/client/src/pages/Home/Search.tsx
+++ apps/client/src/pages/Home/Search.tsx
@@ -1,5 +1,40 @@
-function Search() {
-  return <div className="bg-surface-alt w-52 h-10"></div>;
+import IconButton from '@/components/IconButton';
+import { SearchIcon } from '@/components/Icons';
+import { useForm } from 'react-hook-form';
+
+interface SearchProps {
+  onSearch: (keyword: string) => void;
+}
+
+interface FormInput {
+  keyword: string;
+}
+
+function Search({ onSearch }: SearchProps) {
+  const { register, handleSubmit, reset } = useForm<FormInput>();
+
+  const hanldeSearchSubmit = ({ keyword }: FormInput) => {
+    onSearch(keyword);
+    reset();
+  };
+
+  return (
+    <div className="flex flex-row justify-between items-center w-80 h-10">
+      <form
+        onSubmit={handleSubmit(hanldeSearchSubmit)}
+        className="flex flex-row h-full w-full border border-1 border-border-bold rounded-circle pl-5 pr-2"
+      >
+        <input
+          {...register('keyword', { required: '검색할 방 제목을 입력해주세요' })}
+          className="flex-1 bg-transparent focus-visible:outline-none"
+          placeholder="검색할 방 제목을 입력해주세요"
+        />
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+      </form>
+    </div>
+  );
 }
 
 export default Search;
