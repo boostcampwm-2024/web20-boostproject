95d8685e3c71418b881463b9a37bcab005b6bc62
zero0205 <etety39@naver.com>
2024-12-05 23:21:45 +0900

    :bug: [Fix]: getUserMedia 비디오 옵션 수정

--- apps/client/src/hooks/useMedia.ts
+++ apps/client/src/hooks/useMedia.ts
@@ -13,12 +13,7 @@ export const useMedia = () => {
   const initializeStream = async () => {
     try {
       const mediaStream = await navigator.mediaDevices.getUserMedia({
-        video: {
-          width: { ideal: 1920 },
-          height: { ideal: 1080 },
-          aspectRatio: { ideal: 16 / 9 },
-          facingMode: { ideal: 'user' },
-        },
+        video: true,
         audio: true,
       });
 
