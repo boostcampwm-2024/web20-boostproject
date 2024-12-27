d7ef27bb77f6944b8c1b40fd3d0f58fc4186ef3d
zero0205 <etety39@naver.com>
2024-12-27 16:55:30 +0900

    :sparkles: [Feat]: 전역적으로 테마를 변경할 수 있는 플로팅 버튼 구현
    - 플로팅 버튼 UI 구현
    - Context API로 테마 전역 상태 관리
    - useTheme 커스텀 훅 구현

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,17 +1,22 @@
 import { Outlet } from 'react-router-dom';
 import Header from '@components/Header';
 import './App.css';
-import { AuthProvider } from './contexts/AuthContext';
-import { Toaster } from './components/ui/toaster';
+import { AuthProvider } from '@contexts/AuthContext';
+import { Toaster } from '@components/ui/toaster';
+import FloatingButton from '@components/FloatingButton';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 function App() {
   return (
     <AuthProvider>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Toaster />
+      <ThemeProvider>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Toaster />
+        <FloatingButton />
+      </ThemeProvider>
     </AuthProvider>
   );
 }
